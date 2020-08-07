40d8ab93ad67a4dffa1e495e86967eb7c5e3b174
leochocolat <l.mouraire@outlook.fr>
2020-08-07 18:32:10 +0200

    remove form debug

--- src/javascript/components/GameManager.js
+++ src/javascript/components/GameManager.js
@@ -31,8 +31,8 @@ class GameManager {
         this.progressBarCompletion = document.querySelectorAll('.progress-completion');
         this.looseComponent = new Loose(document.querySelector('.js-section-loose'));
 
-        this.form = new Form(document.querySelector('.js-form-component'));
-        this.form.transitionIn();
+        // this.form = new Form(document.querySelector('.js-form-component'));
+        // this.form.transitionIn();
     }
 
     _setup() {
